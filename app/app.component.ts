4d4bf34d3006a06a0f1704583ffa5d21a18cfc10
Kier Draven <kier.draven@energyaustralia.com.au>
2020-09-02 11:31:34 +1000

    Initial commit.

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "test-app",
+    templateUrl: "app.component.html",
+    styles: ["app.scss"],
+})
+export class AppComponent {}
---
+++ app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { Trace } from "@nativescript/core";
+import { AppComponent } from "~/app.component";
+
+Trace.enable();
+
+@NgModule({
+    bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [],
+    providers: [],
+    schemas: [NO_ERRORS_SCHEMA],
+})
+export class AppModule { }
